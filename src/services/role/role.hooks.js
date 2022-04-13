b57891a3bf43cff815db17d8a64c5f68ecf2435d
Ubuntu <ubuntu@ip-172-31-30-93.ap-south-1.compute.internal>
2022-04-13 07:02:48 +0000

    rol

--- src/services/role/role.hooks.js
+++ src/services/role/role.hooks.js
@@ -16,13 +16,13 @@ const { authenticate } = require('@feathersjs/authentication').hooks;
 
 module.exports = {
   before: {
-    all: [authenticate('jwt')],
-    find: [authenticate('jwt')],
-    get: [authenticate('jwt')],
-    create: [authenticate('jwt') ],
-    update: [authenticate('jwt')],
-    patch: [authenticate('jwt')],
-    remove: [authenticate('jwt')]
+    all: [],
+    find: [],
+    get: [],
+    create: [ ],
+    update: [],
+    patch: [],
+    remove: []
   },
 
   after: {
